adeaea1a43cbf11d45e249c0a0a685fa194f44be
dev-ideausher <developer1@ideausher.com>
2023-11-08 11:40:17 +0530

    first comoit

---
+++ activities/src/bin/a1.rs
@@ -0,0 +1,15 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+fn main() {
+
+
+    ////testing github
+}
